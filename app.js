7ff1e05d3af41600c788eb3788b85ab4f40f8b54
gbhutra <mail.gbhutra@gmail.com>
2016-10-15 18:31:27 -0500

    Linear branch modifications

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var fileUpload = require('express-fileupload');
+//var fileUpload = require('express-fileupload');
 
 
 var routes = require('./routes/index');
@@ -21,10 +21,10 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(fileUpload());
+//app.use(fileUpload());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'inventory')));
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var rfidRdr = require('../middleware/rfidReader').reader;
+var contrl = require('../controller')
+var rfidRdr = contrl.reader;
 //var logger = require('../file_handler');
-var gps = require('../middleware/gpsReader.js').gps;
 
 var log = require('../systemLog.js').routeIndex;
 
@@ -11,118 +11,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'RFID Highway Application',reader: rfidRdr});
 });
 
-router.get("/scanner/:loc", function(req, res) {
-	if (log) console.log("routes/index.js: /scanner set location for reader: "+rfidRdr.getLocation());
-	if (req.params.loc==rfidRdr.getLocation())	{
-		if (log) console.log("routes/index.js: same location set in view : "+rfidRdr.getLocation());
-		res.render("scanner",{reader: rfidRdr});
+router.get("/scanner/", function(req, res) {
+	if (log) console.log("routes/index.js: /scanner for reader");
+	res.render("scanner",{reader: rfidRdr});
+});
+
+router.get("/scannerInit/:maxTags", function(req, res) {
+	if ('ACTVE'!=rfidRdr.status)	{
+		contrl.setMaxTags(req.params.maxTags);
+		res.render("scannerInit",{maxTags: req.params.maxTags, date: Date()});
 	}
 	else	{
-		console.error("error opening the reader page !! Another scan in progress...");
-		if (log) console.log(rfidRdr);
-		var d = new Date();
-		if (log) console.log("routes/index.js: /scanner/ IDLE state ");
-		res.render("scannerInit",{loc: req.params.loc,date: d.toDateString(), GPSLat: gps.currLat, GPSLon: gps.currLon, error: "Incorrect Location seleted !! "});
+		res.render("scannerInit",{maxTags: req.params.maxTags, date: Date(),error: "Scaning in Progress "});
 	}
 });
 
-router.get("/scannerInit/:loc", function(req, res) {
-	if (null!=rfidRdr.getLocation() && req.params.loc != rfidRdr.getLocation())	{
-		var d = new Date();
-		if (log) console.log("routes/index.js: /scannerInit/ different location set in view : "+rfidRdr.getLocation());
-		if ('ACTIVE'==rfidRdr.status)	{
-			console.error("error opening the reader page !! Another scan in progress...");
-			if (log) console.log(rfidRdr);
-			res.render("scannerInit",{loc: req.params.loc,date: d.toDateString(), GPSLat: gps.currLat, GPSLon: gps.currLon,error: "Another scan is in progress !! "});
-		}
-		else	{
-			if (log) console.log("routes/index.js: Reader not in ACTIVE state ");
-			if (log) console.log(rfidRdr);
-			res.render("scannerInit",{loc: req.params.loc,date: d.toDateString(), GPSLat: gps.currLat, GPSLon: gps.currLon});
-		}
-	}
-	else	{
-		var d = new Date();
-		if ('ACTIVE'!=rfidRdr.status) {
-			if (log) console.log("routes/index.js:  /initilalize/ reader not in ACTIVE state ");
-			if (log) console.log(rfidRdr);
-			res.render("scannerInit",{loc: req.params.loc,date: d.toDateString(), GPSLat: gps.currLat, GPSLon: gps.currLon});
-		}
-		else	{
-			console.error("routes/index.js: This is a weired case in /ScannerInit/:loc");
-			if (log) console.log(rfidRdr);
-		}
-	}
-});
-
-
-/*
-router.get("/initialize/:loc", function(req, res) {
-	if (null!=view.loc && req.params.loc != view.loc)	{
-		if (log) console.log("routes/index.js: /initilalize/ different location set in view : "+view.loc);
-		if (1==view.status)	{
-			console.error("error opening the reader page !! Another scan in progress...");
-			var d = new Date();
-			if (log) console.log("routes/index.js:  /initilalize/ reader in IDLE state ");
-			res.render("initialization",{loc: req.params.loc,date: d.toDateString(), GPSLat: gps.currLat, GPSLon: gps.currLon,error: "Another scan is in progress !! "});
-		}
-		else {
-			var d = new Date();
-			if (0==view.status)	{
-				if (log) console.log("routes/index.js:  /initilalize/ reader in IDLE state ");
-				view.loc = req.params.loc;
-				res.render("initialization",{loc: req.params.loc,date: d.toDateString(), GPSLat: gps.currLat, GPSLon: gps.currLon});
-			}
-			else	{
-				if (log) console.log("routes/index.js:  /initilalize/ reader in ERROR state ");
-				view.UpdateStatusTo('stop');
-				res.render("initialization",{loc: req.params.loc,date: d.toDateString(), GPSLat: gps.currLat, GPSLon: gps.currLon,error: "Could not start the reader. Check connections !"});
-			}
-		}
-	}
-	else {
-		if (null==view.loc)	{
-			if (log) console.log("routes/index.js:  /initilalize/ location set as null in view");
-			view.loc = req.params.loc;
-		}
-		if (log) console.log("routes/index.js:  /initilalize/ and same location set in view : "+view.loc+" req: "+req.params.loc);
-		if (1==view.status)	{
-			if (log) console.log("routes/index.js:  /initilalize/ reader in ACTIVE state ");
-			res.render("rfidReader",{view});
-		}
-		else {
-			var d = new Date();
-			if (0==view.status)	{
-				if (log) console.log("routes/index.js:  /initilalize/ reader in IDLE state ");
-				view.loc = req.params.loc;
-				res.render("initialization",{loc: req.params.loc,date: d.toDateString(), GPSLat: gps.currLat, GPSLon: gps.currLon});
-			}
-			else	{
-				if (log) console.log("routes/index.js:  /initilalize/ reader in ERROR state ");
-				view.UpdateStatusTo('stop');
-				res.render("initialization",{loc: req.params.loc,date: d.toDateString(), GPSLat: gps.currLat, GPSLon: gps.currLon,error: "Could not start the reader. Check connections !"});
-			}
-		}
-	}
-});
-
-/*
-router.post('/tagCreate/', function(req,res,next)	{
-	var tag = model.Tag.build({
-			area 		: req.body.area;
-			signType	: req.body.signType,
-			address 	: req.body.address,
-			assetId 	: req.body.assetId,
-			tagId 		: req.body.tagId,
-			lat 		: req.body.lat,
-			lon			: req.body.lon,
-	});
-	return model.Tag.destroy({where: {tagId:req.params.id}}).then(function(affectedRows){
-		console.log("Number of rows deleted :"+affectedRows);
-		model.Tag.findAll({where: {area: req.body.area }, raw: true}).then(function(tags){
-			res.render('tagList',{area:req.body.area, tags:tags});
-		});
-	});
-});*/
 
 module.exports = router;
